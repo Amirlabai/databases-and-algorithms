05459fd97cabe0d3125353fab06358a7bca11a82
Amirlabai <amirlabay@gmail.com>
2025-04-22 18:36:35 +0300

    week 5
    file and folder storage

--- week 5/binary_tree_skelaton_empty.py
+++ week 5/binary_tree_skelaton_empty.py
@@ -17,33 +17,68 @@ class Node():
 
     # return the left child
     def Left(self):
-        pass
+        return self.left
     # return the right child
     def Right(self):
-        pass
+        return self.right
 
     # set node to be the left child of current node
     def set_left(self,node):
-        pass
+        if self.left is None:
+            self.left = node
+            self.left.parent = self
+        else:
+            print("Error: Left child already exists for node with value:", self.value)
 
     # set node to be the right child of current node
     def set_right(self,node):
-        pass
+        if self.right is None:
+            self.right = node
+            if node is not None:
+                node.parent = self
+        else:
+            print("Error: Right child already exists for node with value:", self.value)
 
     # a node is visited after the left child and before thr right child
     def inorder(self):
-        pass
+        result = []
+        # Traverse left subtree
+        if self.left:
+            result.extend(self.left.inorder())
+        # Visit root node
+        result.append(self.value)
+        # Traverse right subtree
+        if self.right:
+            result.extend(self.right.inorder())
+        return result
 
     # a node is visited before its children
     # afterwards, the left child and then the right child
     def PreOrder(self):
-        pass
+        result = []
+        # Visit root node
+        result.append(self.value)
+        # Traverse left subtree
+        if self.left:
+            result.extend(self.left.PreOrder())
+        # Traverse right subtree
+        if self.right:
+            result.extend(self.right.PreOrder())
+        return result
 
     # the left child first
-    #then the right child and the father at
-    # the end
+    #then the right child and the father at the end
     def PostOrder(self):
-        pass
+        result = []
+        # Traverse left subtree
+        if self.left:
+            result.extend(self.left.PostOrder())
+        # Traverse right subtree
+        if self.right:
+            result.extend(self.right.PostOrder())
+        # Visit root node
+        result.append(self.value)
+        return result
 
 
 tree = Node(4)
